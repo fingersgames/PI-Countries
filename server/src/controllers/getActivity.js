d0e752df32177d6382c1f435c3723d2a0757f9a5
Daniel <danitelo@gmail.com>
2023-06-23 17:06:33 -0300

    v3

--- server/src/controllers/getActivity.js
+++ server/src/controllers/getActivity.js
@@ -1,10 +1,13 @@
 
 const {Activity} = require("../db")
+const {country_activity}= require ("../db")
 const getActivity = async (req,res)=>{
     try {
-        const f= await Activity.findAll()
-        if(!f.length) return res.status(400).send('No hay actividades')
-        return res.json(f);
+        const activities= await Activity.findAll()
+        const c_activity= await country_activity.findAll()
+        const r=[activities,c_activity]
+        if(!activities.length) return res.send('No hay actividades')
+        return res.json(r);
     } catch (error) {
         return res.status(500).json(error.message)
     }
--- server/src/controllers/postActivity.js
+++ server/src/controllers/postActivity.js
@@ -1,21 +1,30 @@
 const {Activity} = require('../db')
 const {Country} =require('../db')
 const postActivity = async (req,res)=>{
-
+        console.log('________________________________________')
         const {name, difficulty, duration, season, countries} = req.body
-        
+        let countriesF=countries
         if(name&&difficulty>0&&difficulty<6&&(season==='Verano'||season==='Invierno'||season==='Otoño'||season==='Primavera')&&countries.length!=0){//VER COUNTRI
             const idPais=await Country.findAll({where:{id:countries}})
-            if (countries.length !== idPais.length) return res.status(400).send('No se encontraron pais/es')
-            
+            if (countries.length !== idPais.length) return res.status(400).send('Pais/es incorrecto/s')
             const act={name,difficulty,duration,season}
-            const f=await Activity.findOrCreate({where:act})
-            
-            if(!f[f.length-1]) return res.status(401).send('Ya existe actividad')
-            
+            const [activity, created]=await Activity.findOrCreate({where:act})
+            if(!created) {
+                // countriesId=await country_activity.findAll({where:{CountryId:f.id}})
+                // const countryIds = countriesId.map(e => e.CountryId);
+                // countries = countries.filter(country => !countryIds.includes(country));
+                const countriesA = await activity.getCountries();
+                countriesF = countriesA.map(e => e.id);
+                console.log('countryIDs',countriesF)
+                countriesF = countries.filter(country => !countriesF.includes(country));
+                console.log('countryIDs',countriesF)
+
+            }
             try {
-                await f[0].addCountries(countries)
-                return res.send('Actividad Creada')
+                if (countriesF.length!=0){
+                    await activity.addCountries(countriesF)
+                    return res.send('Actividad Creada')
+                } else return res.status(402).send('Ya existe actividad')
             }
             catch (error) {
                 return res.status(400).send(error.message)
